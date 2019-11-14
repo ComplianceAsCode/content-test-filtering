a7cd3fcedc623c79b3701f142fd9f6d2560a2330
Milan Lysonek <mlysonek@redhat.com>
2019-11-14 13:09:02 +0100

    Introduce parsing of arguments.

---
+++ content-test-filtering.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import sys
+
+import ctf.cli
+
+if __name__ == '__main__':
+    options = ctf.cli.parse_args()
---
+++ ctf/cli.py
@@ -0,0 +1,31 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    common_parser = argparse.ArgumentParser(add_help=False)
+
+    common_parser.add_argument("--base", dest="base_branch", default="master",
+                               help=("Base branch against which we want to "
+                                     "compare, default is \"master\" branch"))
+
+    subparsers = parser.add_subparsers(dest="subcommand",
+                                       help="Subcommands: pr, branch")
+    subparsers.required = True
+
+    parser_pr = subparsers.add_parser("pr", parents=[common_parser],
+                                      help=("Compare base against selected "
+                                            "pull request"))
+    parser_pr.add_argument("target", metavar="PR_NUMBER",
+                           help=("Pull request number, which we want compare "
+                                 "against base"))
+    #parser_pr.set_defaults(func...)
+    parser_branch = subparsers.add_parser("branch", parents=[common_parser],
+                                          help=("Compare base against "
+                                                "selected branch"))
+    parser_branch.add_argument("target", metavar="BRANCH_NAME",
+                           help=("Branch, which we want compare "
+                                 "against base"))
+
+    options = parser.parse_args()
