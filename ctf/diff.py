d6a8b485554f7e1d1c7866ddcb8d3efee29b7d5e
Milan Lysonek <mlysonek@redhat.com>
2019-11-27 16:53:47 +0100

    Change approach of getting branch history.

--- ctf/diff.py
+++ ctf/diff.py
@@ -25,6 +25,8 @@ def get_git_diff_files(options):
         Repo.clone_from(URL, repo_path)
 
     repo = Repo(repo_path)
+    repo.git.checkout(base_branch)
+    repo.remotes.origin.pull()
 
     for r in repo.remotes:
         if re.search(URL, r.url):
@@ -34,26 +36,52 @@ def get_git_diff_files(options):
     logger.info("Fetching branch is %s" % fetch_branch)
     # Checkout to master, so we can fetch, even if the branch exist - it is not
     # possible to fetch, when we are on the same branch
-    repo.git.checkout(base_branch)
     remote.fetch("pull/" + pr_number + "/head:" + fetch_branch, force=True)
     repo.git.checkout(fetch_branch)
-    # git log --name-status --oneline master..HEAD
-    git_log = repo.git.log("--name-status", "--format=\"%H\"", base_branch + "..HEAD")
-    file_list = []
+    git_log_old = repo.git.log("--format=%H", "master")
+    git_log_old = git_log_old.split()
+    git_log_new = repo.git.log("--format=%H")
+    git_log_new = git_log_new.split()
+    for i in git_log_new:
+        if i in git_log_old:
+            print(i)
+            break
+    # repo.git.checkout(fetch_branch)
+    # repo.git.checkout(base_branch)
+    #git_log = repo.git.log("master", "^" + fetch_branch, "--ancestry-path",
+    #                       "--format=\"%P\"", "--reverse")
+    # if git_log:
+    #     print(git_log)
+    #     merge_base = git_log.partition("\n")[0]
+    #     merge_base = merge_base.replace("\"","")
+    #     merge_base = merge_base.split(" ")
+    #     print(merge_base)
+    #     # git log --name-status --oneline master..HEAD
+    #     # git_base_commit = repo.git.log("--name-status", "--format=\"%H\"", base_branch + "..HEAD")
+    #     git_base_commit = repo.git.merge_base("--all", merge_base)
+    # else:
+    #     git_base_commit = base_branch + "."
+    # repo.git.checkout(fetch_branch)
+    # git_diff = repo.git.diff("--name-status", git_base_commit + "..HEAD")
+    # print(git_diff)
+    # file_list = []
 
     # rev_list_before = repo.git.rev_list('master')
     # rev_list_after = repo.git.rev_list('--first-parent HEAD')
     # logger.info(rev_list_before)
     # M,A,D,Rxxx
-    logger.info(git_log)
-    for git_line in git_log.splitlines():
+    #logger.info(git_log)
+    for git_line in git_diff.splitlines():
         line = git_line.split("\t")
         flags_regexp = re.compile(r'^[M|A|D|R\d{3}|C\d{3}]')
         if flags_regexp.search(line[0]):
             if line[0] != 'A':
-                file_before = repo.git.show(base_branch + ":./" + line[1])
+                file_before = repo.git.show(git_base_commit + ":./" + line[1])
             if line[0] != 'D':
                 file_after = repo.git.show("HEAD:./" + line[1])
+            print(file_before)
+            print("----------------------------------------------")
+            print(file_after)
             line.append(file_before)
             line.append(file_after)
             line[1] = repo_path + "/" + line[1]
