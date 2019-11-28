5abb0ebb4d55f6b758c78baa6b5751040dac4792
Milan Lysonek <mlysonek@redhat.com>
2019-11-28 14:16:21 +0100

    Remove commented code at diff.py.

--- ctf/diff.py
+++ ctf/diff.py
@@ -24,7 +24,7 @@ def fetch_branch(remote, branch_name):
 def get_git_diff_files(options):
     base_branch = options.base_branch
     repo_path = options.repository_path
-
+    changed_files = []
 
     # Create a new temporary dictory if it does not exist
     if repo_path is not None and not os.path.isdir(repo_path):
@@ -78,54 +78,22 @@ def get_git_diff_files(options):
         compare_commit = common_commit
 
     git_diff = repo.git.diff("--name-status", compare_commit + "..HEAD")
-    print(git_diff)
-    exit()
-    # repo.git.checkout(fetch_branch)
-    # repo.git.checkout(base_branch)
-    #git_log = repo.git.log("master", "^" + fetch_branch, "--ancestry-path",
-    #                       "--format=\"%P\"", "--reverse")
-    # if git_log:
-    #     print(git_log)
-    #     merge_base = git_log.partition("\n")[0]
-    #     merge_base = merge_base.replace("\"","")
-    #     merge_base = merge_base.split(" ")
-    #     print(merge_base)
-    #     # git log --name-status --oneline master..HEAD
-    #     # git_base_commit = repo.git.log("--name-status", "--format=\"%H\"", base_branch + "..HEAD")
-    #     git_base_commit = repo.git.merge_base("--all", merge_base)
-    # else:
-    #     git_base_commit = base_branch + "."
-    # repo.git.checkout(fetch_branch)
-    # git_diff = repo.git.diff("--name-status", git_base_commit + "..HEAD")
-    # print(git_diff)
-    file_list = []
 
-    # rev_list_before = repo.git.rev_list('master')
-    # rev_list_after = repo.git.rev_list('--first-parent HEAD')
-    # logger.info(rev_list_before)
     # M,A,D,Rxxx
-    #logger.info(git_log)
     for git_line in git_diff.splitlines():
-        line = git_line.split("\t")
-        # flags_regexp = re.compile(r'^[M|A|D|R\d{3}|C\d{3}]')
-        # if flags_regexp.search(line[0]):
+        file_record = dict()
         file_before = None
-        if line[0] != 'A':
-            file_before = repo.git.show(compare_commit + ":./" + line[1])
-        if line[0] != 'D':
-            file_after = repo.git.show("HEAD:./" + line[1])
-            # if file_before:
-            #     print(file_before)
-            # print("^BEFORE----------------------------------------------AFTER")
-            # print(file_after)
-        line.append(file_before)
-        line.append(file_after)
-        line[1] = repo_path + "/" + line[1]
-        file_list.append(line)
+        file_after = None
 
-    return file_list
-    #print(repo.git.log("master..HEAD", name-status=True))
-    #commits = list(repo.iter_commits("master..HEAD"))
-    #for c in commits:
-    #    print(c.stats)
-    #    print(inspect.getmembers(c.stats))
+        flag, filepath = git_line.split("\t")  # Parse flag and filepath
+        if flag != 'A':  # If file wasn't added, we have previous version
+            file_before = repo.git.show(compare_commit + ":./" + filepath)
+        if flag != 'D':  # If file wasn't deleted, we have new version
+            file_after = repo.git.show("HEAD:./" + filepath)
+
+        file_record["flag"] = flag
+        file_record["file_before"] = file_before
+        file_record["file_after"] = file_after
+        changed_files.append(file_record)
+
+    return changed_files
