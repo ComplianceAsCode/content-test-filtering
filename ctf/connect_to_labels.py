bfd95d1aad7c83ab70622cbe73227c8d472fe4f3
Milan Lysonek <mlysonek@redhat.com>
2020-03-23 18:41:22 +0100

    Remove usage of ruamel.yaml and use instead PyYAML.

--- ctf/connect_to_labels.py
+++ ctf/connect_to_labels.py
@@ -1,7 +1,7 @@
 import logging
 import jinja2
+import yaml
 from pathlib import Path
-from ruamel.yaml import YAML
 
 
 logger = logging.getLogger("content-test-filtering.connect_to_labels")
@@ -10,14 +10,13 @@ TEST_LABELS = "test_labels.yml"
 
 
 def get_labels(content_tests):
-    yaml = YAML(typ="safe")
     template_loader = jinja2.FileSystemLoader(
                         searchpath=str(Path(__file__).parent / ".."))
     template_env = jinja2.Environment(loader=template_loader)
     tests = []
 
     for product in content_tests.products_affected:
-        yaml_content = yaml.load(template_env.get_template(TEST_LABELS).render(
+        yaml_content = yaml.safe_load(template_env.get_template(TEST_LABELS).render(
             product=product
         ))
 
