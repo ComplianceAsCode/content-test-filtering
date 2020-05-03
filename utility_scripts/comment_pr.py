d7201146493d073b337c5119064eb5d8bd8cd467
Milan Lysonek <mlysonek@redhat.com>
2020-05-03 13:24:58 +0200

    Add utility script that deletes old comments about findings and posts new one.

---
+++ utility_scripts/comment_pr.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import argparse
+import requests
+import json
+import sys
+
+REPO_URL = "https://api.github.com/repos/mildas/content-test-filtering/issues/"
+USER = "mildas"
+COMMENT_STRING = "Findings:"
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--token", dest="gh_auth", required=True,
+                        help=("Authorization token for comments at Github."))
+    parser.add_argument("--pr", dest="pr", required=True,
+                        help=("Pull Request number where to send the comment."))
+    parser.add_argument("--comment", dest="comment_file", required=True,
+                        help=("File with content that will be send as comment to the Pull Request."))
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    options = parse_args()
+    # Prepare method's specifications
+    headers = {"Authorization": "token %s" % options.gh_auth}
+    data = {}
+    with open(options.comment_file) as comment:
+        data["body"] = comment.read()
+    if not data["body"]: # If the comment is empty -> don't delete/send anything
+        sys.exit()
+    data = json.dumps(data)
+    add_comment_url = REPO_URL + options.pr + "/comments"
+    # Get all comments from the Pull Request
+    response = requests.get(add_comment_url)
+    parsed_comments = json.loads(response.content)
+    # Delete comments with specific comment string
+    for comment in parsed_comments:
+        if comment["user"]["login"] == USER and COMMENT_STRING in comment["body"]:
+            remove_comment_url = REPO_URL + "comments/" + str(comment["id"])
+            response = requests.delete(remove_comment_url, headers=headers)
+    # Send the comment
+    response = requests.post(add_comment_url, data=data, headers=headers)
